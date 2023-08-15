6e27b7780e28dac1a885eb5e2fdc8b876f8bdaea
mu1978jun <108204498+mu1978jun@users.noreply.github.com>
2023-08-15 21:43:21 +0800

    Update 夜猫至臻.js

--- CC/夜猫至臻.js
+++ CC/夜猫至臻.js
@@ -18,7 +18,7 @@ var rule = {
 	filterable:1,//是否启用分类筛选,
 	headers:{'User-Agent':'Dart/2.14 (dart:io)'},
 	timeout:5000,
-	class_name:'电影&国产电视剧&电视剧综合&港台剧&日韩剧&欧美剧&海外剧&泰国剧&综艺', // 分类筛选 /api.php/app/nav
+	class_name:'动漫&电影&国产电视剧&电视剧综合&港台剧&日韩剧&欧美剧&海外剧&泰国剧&综艺', // 分类筛选 /api.php/app/nav
 	class_url:'3&1&20&26&21&22&23&24&25&4',
 	play_parse:true,
 	lazy:'js:if(/m3u8|mp4/.test(input)){input}else if(/YuMi/.test(input)){let purl=request("http://123.99.192.241:5000/api/jiexi/common?Key=NCVOW2aG7aaDnnVakQ&url="+input);input={jx:0,url:JSON.parse(purl).url,parse:0}}else if(/YuMi/.test(input)){let purl=request("http://123.99.192.241:5000/api/jiexi/xg?Key=NLoNjn48GAF3UQdsP0&url="+input);input={jx:0,url:JSON.parse(purl).url,parse:0}}else if(/qiqi/.test(input)){let purl=request("http://42.51.37.161:5678/json/520238app8/ml77.php?url="+input);input={jx:0,url:JSON.parse(purl).url,parse:0}}',
