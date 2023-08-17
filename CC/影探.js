c1d8967fc424acc0397d7e0a18b4392d02c280ec
mu1978jun <108204498+mu1978jun@users.noreply.github.com>
2023-08-17 21:26:16 +0800

    Update and rename 影探V2.js to 影探.js

--- CC/影探.js
+++ CC/影探.js
@@ -49,8 +49,11 @@ try {
 var rule = {
 	title: '影探[V2]', // csp_AppYsV2
 	//host: 'http://ytcms.lfytyl.com',
-	host: 'http://long123.lfytyl.com/api.php/app',
+	
     // hostJs:'print(HOST);let html=JSON.parse(request(HOST,{headers:{"User-Agent":PC_UA}}));let src = html.sites[0].ext.replace("/api.php/app/","");print(src);HOST=src',//网页域名根动态抓取js代码。通过HOST=赋值
+    host: 'https://download.kstore.space/download/3313/mao/0.json',
+	//host: 'http://ytcms.lfytyl.com',
+  hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":"Dart/3.0 (dart:io)"}});let src = JSON.parse(html).domain;print(src);HOST=src',
 	homeUrl:'/api.php/app/index_video',
 	url: '/api.php/app/video?tid=fyclassfyfilter&limit=18&pg=fypage',
 	filter_url:'&class={{fl.class}}&area={{fl.area}}&lang={{fl.lang}}&year={{fl.year}}',
@@ -81,8 +84,7 @@ var rule = {
 	class_name:'新电影4K&新剧4K&好莱坞4K&豆瓣Top250电影4K&豆瓣Top100影视剧4K&港台经典4k&少儿动画4K&邵氏老电影4K&明星专辑4K&六公主4K&电影综合&电视剧综合&动漫&综艺&体育赛事', // 分类筛选 /api.php/app/nav
 	class_url:'20&21&47&49&46&48&50&5&45&44&1&2&4&3&32',
 	play_parse:true,
-	play_json:0,
-		lazy:'js:input=/m3u8|mp4/.test(input)?input:{jx:0,url:"http://lflong.lyyytv.cn/json.php?url="+input,parse:1}',
+		lazy:'js:if(/m3u8|mp4/.test(input)){input}else if(/XY/.test(input)){let purl=request("http://lfty.lyyytv.cn/json.php?url="+input);input={jx:0,url:JSON.parse(purl).url,parse:0}}',
 	limit:6,
 	推荐:'json:list;vlist;*;*;*;*',
 	double: true,
